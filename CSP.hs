17f55c2c4e8bf2d006df8f6b0d0e4117b33faecc
Eric Walkingshaw <eric@walkingshaw.net>
2013-09-19 11:04:19 +0200

    Move n-Queens problem into separate module.

--- CSP.hs
+++ CSP.hs
@@ -99,35 +99,3 @@ eq a b vs = (not . null) (da `intersect` db)
 pairwise :: (Variable n a -> Variable n a -> Bool) -> Constraint n a
 pairwise f []     = True
 pairwise f (v:vs) = all (f v) vs && pairwise f vs
-
-
--- * Example: N-Queens Problem
-
-type Row = Int
-type Col = Int
-
--- | A variable in the n-queens problem, representing a single queen.
---   The variable's name is the queen's row, while its column is the value to
---   to be assigned.
-type Queen = Variable Row Col
-
--- | 'True' if the two queens can attack each other.
---   That is, if they are on the same row, column, or diagonal.
-canAttack :: Queen -> Queen -> Bool
-canAttack (r,[c]) (s,[d]) = r == s || c == d || abs (r-s) == abs (c-d)
-canAttack _ _ = False
-
--- | 'True' if the two queens cannot attack each other.
-safe :: Queen -> Queen -> Bool
-safe = (not .) . canAttack
-
--- | A constraint indicating that all queens are pairwise safe.
-allSafe :: [Queen] -> Bool
-allSafe = pairwise safe
-
--- | Find all solutions to the n-queens problem, for a given value of end.
---   'head' can be used to get a single solution only, for example,
---   @head (queens 20)@.
-queens :: Int -> [[Queen]]
-queens n = solve [allSafe] qs
-  where qs = [(r,[1..n]) | r <- [1..n]]
